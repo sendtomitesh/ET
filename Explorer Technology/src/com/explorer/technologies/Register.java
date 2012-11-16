b00c4499cbca17c802467b4c0cf13eb1ea495069
Antarix <antarix.tandon@gmail.com>
2012-11-16 14:18:16 +0530

    Updated Register Layout

--- Explorer Technology/src/com/explorer/technologies/Register.java
+++ Explorer Technology/src/com/explorer/technologies/Register.java
@@ -2,7 +2,10 @@ package com.explorer.technologies;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Register extends Activity {
 
@@ -13,5 +16,16 @@ public class Register extends Activity {
         
         
     }
-           
+     
+    public void getRegister(View v)
+    {
+    	Toast.makeText(getApplicationContext(), "Registered!", Toast.LENGTH_LONG).show();
+    }
+    
+    public void gotoLogin(View v)
+    {
+    	Intent loginIntent = new Intent(getApplicationContext(),Login.class);
+    	startActivity(loginIntent);
+    	finish();
+    }
 }
